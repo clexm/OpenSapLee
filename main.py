2f59a6a78dc78f0a7cb9e80693197e908224e7ae
clexm <51886140+clexm@users.noreply.github.com>
2026-06-12 17:03:42 +0800

    cedf

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ SUBSCRIBE_RAW_URLS = [
     "https://raw.githubusercontent.com/free-nodes/v2rayfree/main/v202606122",
     "https://raw.githubusercontent.com/free18/v2ray/refs/heads/main/v.txt",
     "http://59.33.33.157:18080/v2ray.txt",
+    "https://github.com/chengaopan/AutoMergePublicNodes/blob/master/list.txt",
+    "https://ghfast.top/https://raw.githubusercontent.com/free18/v2ray/refs/heads/main/v.txt",
     "https://raw.githubusercontent.com/shaoyouvip/free/main/base64.txt"
 ]
 
